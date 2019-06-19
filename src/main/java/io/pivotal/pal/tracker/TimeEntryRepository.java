f2713fcf5d18d1d2c992ead11c73347b8ece5885
Santhosh <santhosh_kumar_v_a@dell.com>
2019-06-19 14:30:31 +0530

    jdbc template

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -3,9 +3,9 @@ package io.pivotal.pal.tracker;
 import java.util.List;
 
 public interface TimeEntryRepository {
-    public TimeEntry create(TimeEntry timeEntryimeEntry);
-    public TimeEntry find(long timeEntryId);
-    public List<TimeEntry> list();
-    public TimeEntry update(long eq, TimeEntry any);
-    public void delete(long timeEntryId);
+    TimeEntry create(TimeEntry timeEntryimeEntry);
+    TimeEntry find(long timeEntryId);
+    List<TimeEntry> list();
+    void delete(long timeEntryId);
+    TimeEntry update(long id, TimeEntry timeEntry);
 }
